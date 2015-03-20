3d794a6ac309e9306c3c2b0ad7629bf8ee649438
Julian Viso <julian.viso@outlook.com>
2015-03-19 23:52:13 -0400

    OCR Test
    currently commented out, not finished

--- app/src/main/java/edu/uky/cs/www/diagramaphone/MainActivity.java
+++ app/src/main/java/edu/uky/cs/www/diagramaphone/MainActivity.java
@@ -2,14 +2,23 @@ package edu.uky.cs.www.diagramaphone;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+import java.io.IOException;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -68,3 +77,79 @@ public class MainActivity extends ActionBarActivity {
 
     }
 }
+
+    /*
+    OCR TEST
+     *
+    protected void ocr() {
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = 2;
+        Bitmap bitmap = BitmapFactory.decodeFile(IMAGE_PATH, options);
+
+        try {
+            ExifInterface exif = new ExifInterface(IMAGE_PATH);
+            int exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+
+            Log.v(LOG_TAG, "Orient: " + exifOrientation);
+
+            int rotate = 0;
+            switch (exifOrientation) {
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    rotate = 90;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    rotate = 180;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    rotate = 270;
+                    break;
+            }
+
+            Log.v(LOG_TAG, "Rotation: " + rotate);
+
+            if (rotate != 0) {
+
+                // Getting width & height of the given image.
+                int w = bitmap.getWidth();
+                int h = bitmap.getHeight();
+
+                // Setting pre rotate
+                Matrix mtx = new Matrix();
+                mtx.preRotate(rotate);
+
+                // Rotating Bitmap
+                bitmap = Bitmap.createBitmap(bitmap, 0, 0, w, h, mtx, false);
+                // tesseract req. ARGB_8888
+                bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+            }
+
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Rotate or coversion failed: " + e.toString());
+        }
+
+        ImageView iv = (ImageView) findViewById(R.id.image);
+        iv.setImageBitmap(bitmap);
+        iv.setVisibility(View.VISIBLE);
+
+        Log.v(LOG_TAG, "Before baseApi");
+
+        TessBaseAPI baseApi = new TessBaseAPI();
+        baseApi.setDebug(true);
+        baseApi.init(DATA_PATH, LANG);
+        baseApi.setImage(bitmap);
+        String recognizedText = baseApi.getUTF8Text();
+        baseApi.end();
+
+        Log.v(LOG_TAG, "OCR Result: " + recognizedText);
+
+        // clean up and show
+        if (LANG.equalsIgnoreCase("eng")) {
+            recognizedText = recognizedText.replaceAll("[^a-zA-Z0-9]+", " ");
+        }
+        if (recognizedText.length() != 0) {
+            ((TextView) findViewById(R.id.field)).setText(recognizedText.trim());
+        }
+    }
+
+    */
