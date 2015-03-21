9bf319efbeeb6a0cee8b6f714fad342f47d52c17
Jesse Hall <jessehall198@gmail.com>
2015-03-21 14:50:40 -0400

    completely messed up the app, but may have working ocr

--- app/src/main/java/edu/uky/cs/www/diagramaphone/MainActivity.java
+++ app/src/main/java/edu/uky/cs/www/diagramaphone/MainActivity.java
@@ -1,99 +1,175 @@
 package edu.uky.cs.www.diagramaphone;
 
-import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
-import android.media.ExifInterface;
-import android.net.Uri;
-import android.os.Bundle;
-import android.provider.MediaStore;
-import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.googlecode.tesseract.android.TessBaseAPI;
+        import java.io.File;
+        import java.io.FileInputStream;
+        import java.io.FileOutputStream;
+        import java.io.IOException;
+        import java.io.InputStream;
+        import java.io.OutputStream;
+        import java.util.zip.GZIPInputStream;
 
-import java.io.IOException;
+        import android.app.Activity;
+        import android.content.Intent;
+        import android.content.res.AssetManager;
+        import android.graphics.Bitmap;
+        import android.graphics.BitmapFactory;
+        import android.graphics.Matrix;
+        import android.media.ExifInterface;
+        import android.net.Uri;
+        import android.os.Bundle;
+        import android.os.Environment;
+        import android.provider.MediaStore;
+        import android.util.Log;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
 
+        import com.googlecode.tesseract.android.TessBaseAPI;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+    public static final String PACKAGE_NAME = "com.datumdroid.android.ocr.simple";
+    public static final String DATA_PATH = Environment
+            .getExternalStorageDirectory().toString() + "/SimpleAndroidOCR/";
 
-    private static int RESULT_LOAD_IMG = 1;
-    String imgDecodableString;
+    // You should have the trained data file in assets folder
+    // You can get them at:
+    // http://code.google.com/p/tesseract-ocr/downloads/list
+    public static final String lang = "eng";
+
+    private static final String TAG = "SimpleAndroidOCR.java";
+
+    protected Button _button;
+    // protected ImageView _image;
+    protected EditText _field;
+    protected String _path;
+    protected boolean _taken;
+
+    protected static final String PHOTO_TAKEN = "photo_taken";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
+
+        String[] paths = new String[] { DATA_PATH, DATA_PATH + "tessdata/" };
+
+        for (String path : paths) {
+            File dir = new File(path);
+            if (!dir.exists()) {
+                if (!dir.mkdirs()) {
+                    Log.v(TAG, "ERROR: Creation of directory " + path + " on sdcard failed");
+                    return;
+                } else {
+                    Log.v(TAG, "Created directory " + path + " on sdcard");
+                }
+            }
+
+        }
+
+        // lang.traineddata file with the app (in assets folder)
+        // You can get them at:
+        // http://code.google.com/p/tesseract-ocr/downloads/list
+        // This area needs work and optimization
+        if (!(new File(DATA_PATH + "tessdata/" + lang + ".traineddata")).exists()) {
+            try {
+
+                AssetManager assetManager = getAssets();
+                InputStream in = assetManager.open("tessdata/" + lang + ".traineddata");
+                //GZIPInputStream gin = new GZIPInputStream(in);
+                OutputStream out = new FileOutputStream(DATA_PATH
+                        + "tessdata/" + lang + ".traineddata");
+
+                // Transfer bytes from in to out
+                byte[] buf = new byte[1024];
+                int len;
+                //while ((lenf = gin.read(buff)) > 0) {
+                while ((len = in.read(buf)) > 0) {
+                    out.write(buf, 0, len);
+                }
+                in.close();
+                //gin.close();
+                out.close();
+
+                Log.v(TAG, "Copied " + lang + " traineddata");
+            } catch (IOException e) {
+                Log.e(TAG, "Was unable to copy " + lang + " traineddata " + e.toString());
+            }
+        }
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        // _image = (ImageView) findViewById(R.id.image);
+        _field = (EditText) findViewById(R.id.field);
+        _button = (Button) findViewById(R.id.button);
+        _button.setOnClickListener(new ButtonClickHandler());
+
+        _path = DATA_PATH + "/ocr.jpg";
     }
 
-    public void loadImagefromGallery(View view) {
-        // Create intent to Open Image applications like Gallery, Google Photos
-        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
-                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        // Start the Intent
-        startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
+    public class ButtonClickHandler implements View.OnClickListener {
+        public void onClick(View view) {
+            Log.v(TAG, "Starting Camera app");
+            startCameraActivity();
+        }
+    }
+
+    // Simple android photo capture:
+    // http://labs.makemachine.net/2010/03/simple-android-photo-capture/
+
+    protected void startCameraActivity() {
+        File file = new File(_path);
+        Uri outputFileUri = Uri.fromFile(file);
+
+        final Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+
+        startActivityForResult(intent, 0);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        try {
-            // When an Image is picked
-            if (requestCode == RESULT_LOAD_IMG && resultCode == RESULT_OK
-                    && null != data) {
-                // Get the Image from data
 
-                Uri selectedImage = data.getData();
-                String[] filePathColumn = { MediaStore.Images.Media.DATA };
+        Log.i(TAG, "resultCode: " + resultCode);
 
-                // Get the cursor
-                Cursor cursor = getContentResolver().query(selectedImage,
-                        filePathColumn, null, null, null);
-                // Move to first row
-                cursor.moveToFirst();
-
-                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-                imgDecodableString = cursor.getString(columnIndex);
-                cursor.close();
-                ImageView imgView = (ImageView) findViewById(R.id.imgView);
-                // Set the Image in ImageView after decoding the String
-                imgView.setImageBitmap(BitmapFactory
-                        .decodeFile(imgDecodableString));
-
-            } else {
-                Toast.makeText(this, "You haven't picked Image",
-                        Toast.LENGTH_LONG).show();
-            }
-        } catch (Exception e) {
-            Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG)
-                    .show();
+        if (resultCode == -1) {
+            onPhotoTaken();
+        } else {
+            Log.v(TAG, "User cancelled");
         }
-
     }
-}
 
-    /*
-    OCR TEST
-     *
-    protected void ocr() {
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putBoolean(MainActivity.PHOTO_TAKEN, _taken);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        Log.i(TAG, "onRestoreInstanceState()");
+        if (savedInstanceState.getBoolean(MainActivity.PHOTO_TAKEN)) {
+            onPhotoTaken();
+        }
+    }
+
+    protected void onPhotoTaken() {
+        _taken = true;
 
         BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inSampleSize = 2;
-        Bitmap bitmap = BitmapFactory.decodeFile(IMAGE_PATH, options);
+        options.inSampleSize = 4;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(_path, options);
 
         try {
-            ExifInterface exif = new ExifInterface(IMAGE_PATH);
-            int exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+            ExifInterface exif = new ExifInterface(_path);
+            int exifOrientation = exif.getAttributeInt(
+                    ExifInterface.TAG_ORIENTATION,
+                    ExifInterface.ORIENTATION_NORMAL);
 
-            Log.v(LOG_TAG, "Orient: " + exifOrientation);
+            Log.v(TAG, "Orient: " + exifOrientation);
 
             int rotate = 0;
+
             switch (exifOrientation) {
                 case ExifInterface.ORIENTATION_ROTATE_90:
                     rotate = 90;
@@ -106,7 +182,7 @@ public class MainActivity extends ActionBarActivity {
                     break;
             }
 
-            Log.v(LOG_TAG, "Rotation: " + rotate);
+            Log.v(TAG, "Rotation: " + rotate);
 
             if (rotate != 0) {
 
@@ -120,36 +196,48 @@ public class MainActivity extends ActionBarActivity {
 
                 // Rotating Bitmap
                 bitmap = Bitmap.createBitmap(bitmap, 0, 0, w, h, mtx, false);
-                // tesseract req. ARGB_8888
-                bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
             }
 
+            // Convert to ARGB_8888, required by tess
+            bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+
         } catch (IOException e) {
-            Log.e(LOG_TAG, "Rotate or coversion failed: " + e.toString());
+            Log.e(TAG, "Couldn't correct orientation: " + e.toString());
         }
 
-        ImageView iv = (ImageView) findViewById(R.id.image);
-        iv.setImageBitmap(bitmap);
-        iv.setVisibility(View.VISIBLE);
+        // _image.setImageBitmap( bitmap );
 
-        Log.v(LOG_TAG, "Before baseApi");
+        Log.v(TAG, "Before baseApi");
 
         TessBaseAPI baseApi = new TessBaseAPI();
         baseApi.setDebug(true);
-        baseApi.init(DATA_PATH, LANG);
+        baseApi.init(DATA_PATH, lang);
         baseApi.setImage(bitmap);
+
         String recognizedText = baseApi.getUTF8Text();
+
         baseApi.end();
 
-        Log.v(LOG_TAG, "OCR Result: " + recognizedText);
+        // You now have the text in recognizedText var, you can do anything with it.
+        // We will display a stripped out trimmed alpha-numeric version of it (if lang is eng)
+        // so that garbage doesn't make it to the display.
 
-        // clean up and show
-        if (LANG.equalsIgnoreCase("eng")) {
+        Log.v(TAG, "OCRED TEXT: " + recognizedText);
+
+        if ( lang.equalsIgnoreCase("eng") ) {
             recognizedText = recognizedText.replaceAll("[^a-zA-Z0-9]+", " ");
         }
-        if (recognizedText.length() != 0) {
-            ((TextView) findViewById(R.id.field)).setText(recognizedText.trim());
+
+        recognizedText = recognizedText.trim();
+
+        if ( recognizedText.length() != 0 ) {
+            _field.setText(_field.getText().toString().length() == 0 ? recognizedText : _field.getText() + " " + recognizedText);
+            _field.setSelection(_field.getText().toString().length());
         }
+
+        // Cycle done.
     }
 
-    */
+    // www.Gaut.am was here
+    // Thanks for reading!
+}
