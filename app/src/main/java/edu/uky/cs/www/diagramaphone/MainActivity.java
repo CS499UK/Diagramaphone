2d890cedbfadc412807be81b6e787b4e5d652ea7
LWilliamson1 <larry86_06@hotmail.com>
2015-03-28 18:48:38 -0400

    Committing in a submodule

--- app/src/main/java/edu/uky/cs/www/diagramaphone/MainActivity.java
+++ app/src/main/java/edu/uky/cs/www/diagramaphone/MainActivity.java
@@ -1,6 +1,7 @@
 package edu.uky.cs.www.diagramaphone;
 
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -8,6 +9,7 @@ import android.graphics.Matrix;
 import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
@@ -18,7 +20,11 @@ import android.widget.Toast;
 
 import com.googlecode.tesseract.android.TessBaseAPI;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -26,8 +32,66 @@ public class MainActivity extends ActionBarActivity {
     private static int RESULT_LOAD_IMG = 1;
     String imgDecodableString;
 
+    public static final String PACKAGE_NAME = "com.datumdroid.android.ocr.simple";
+    public static final String DATA_PATH = Environment
+            .getExternalStorageDirectory().toString() + "/Diagramaphone/";
+
+    // You should have the trained data file in assets folder
+    // You can get them at:
+    // http://code.google.com/p/tesseract-ocr/downloads/list
+    public static final String lang = "eng";
+
+    private static final String TAG = "SimpleAndroidOCR.java";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        String[] paths = new String[] { DATA_PATH, DATA_PATH + "tessdata/" };
+
+        for (String path : paths) {
+            File dir = new File(path);
+            if (!dir.exists()) {
+                if (!dir.mkdirs()) {
+                    Log.v(TAG, "ERROR: Creation of directory " + path + " on sdcard failed");
+                    return;
+                } else {
+                    Log.v(TAG, "Created directory " + path + " on sdcard");
+                }
+            }
+
+        }
+
+        // lang.traineddata file with the app (in assets folder)
+        // You can get them at:
+        // http://code.google.com/p/tesseract-ocr/downloads/list
+        // This area needs work and optimization
+        if (!(new File(DATA_PATH + "tessdata/" + lang + ".traineddata")).exists()) {
+            try {
+
+                AssetManager assetManager = getAssets();
+
+                //Verifies that the tessdata folder exists, and there exists a .traineddata file.
+                InputStream in = assetManager.open("tessdata/" + lang + ".traineddata");
+                OutputStream out = new FileOutputStream(DATA_PATH
+                        + "tessdata/" + lang + ".traineddata");
+
+                // Transfer bytes from in to out
+                byte[] buf = new byte[1024];
+                int len;
+                while ((len = in.read(buf)) > 0) {
+                    out.write(buf, 0, len);
+                }
+                in.close();
+                out.close();
+
+                Log.v(TAG, "Copied " + lang + " traineddata");
+            } catch (IOException e) {
+                Log.e(TAG, "Was unable to copy " + lang + " traineddata " + e.toString());
+            }
+        }
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
@@ -75,25 +139,28 @@ public class MainActivity extends ActionBarActivity {
                     .show();
         }
 
+        scanForText(imgDecodableString);
     }
-}
 
-    /*
-    OCR TEST
-     *
-    protected void ocr() {
+
+    protected void scanForText(String imgDecodableString) {
+        //_taken = true;
 
         BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inSampleSize = 2;
-        Bitmap bitmap = BitmapFactory.decodeFile(IMAGE_PATH, options);
+        options.inSampleSize = 4;
 
+        Bitmap bitmap = BitmapFactory.decodeFile(imgDecodableString, options);
+        /*
         try {
-            ExifInterface exif = new ExifInterface(IMAGE_PATH);
-            int exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+            ExifInterface exif = new ExifInterface(_path);
+            int exifOrientation = exif.getAttributeInt(
+                    ExifInterface.TAG_ORIENTATION,
+                    ExifInterface.ORIENTATION_NORMAL);
 
-            Log.v(LOG_TAG, "Orient: " + exifOrientation);
+            Log.v(TAG, "Orient: " + exifOrientation);
 
             int rotate = 0;
+
             switch (exifOrientation) {
                 case ExifInterface.ORIENTATION_ROTATE_90:
                     rotate = 90;
@@ -106,7 +173,7 @@ public class MainActivity extends ActionBarActivity {
                     break;
             }
 
-            Log.v(LOG_TAG, "Rotation: " + rotate);
+            Log.v(TAG, "Rotation: " + rotate);
 
             if (rotate != 0) {
 
@@ -120,36 +187,48 @@ public class MainActivity extends ActionBarActivity {
 
                 // Rotating Bitmap
                 bitmap = Bitmap.createBitmap(bitmap, 0, 0, w, h, mtx, false);
-                // tesseract req. ARGB_8888
-                bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
             }
 
+            // Convert to ARGB_8888, required by tess
+            bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+
         } catch (IOException e) {
-            Log.e(LOG_TAG, "Rotate or coversion failed: " + e.toString());
-        }
+            Log.e(TAG, "Couldn't correct orientation: " + e.toString());
+        }*/
+        bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+        // _image.setImageBitmap( bitmap );
 
-        ImageView iv = (ImageView) findViewById(R.id.image);
-        iv.setImageBitmap(bitmap);
-        iv.setVisibility(View.VISIBLE);
-
-        Log.v(LOG_TAG, "Before baseApi");
+        Log.v(TAG, "Before baseApi");
 
         TessBaseAPI baseApi = new TessBaseAPI();
         baseApi.setDebug(true);
-        baseApi.init(DATA_PATH, LANG);
+        baseApi.init(DATA_PATH, lang);
         baseApi.setImage(bitmap);
+
+        //TODO: This is now the problem line.  Not sure what is going wrong.
         String recognizedText = baseApi.getUTF8Text();
+
         baseApi.end();
 
-        Log.v(LOG_TAG, "OCR Result: " + recognizedText);
+        // You now have the text in recognizedText var, you can do anything with it.
+        // We will display a stripped out trimmed alpha-numeric version of it (if lang is eng)
+        // so that garbage doesn't make it to the display.
 
-        // clean up and show
-        if (LANG.equalsIgnoreCase("eng")) {
+        Log.v(TAG, "OCRED TEXT: " + recognizedText);
+
+        if ( lang.equalsIgnoreCase("eng") ) {
             recognizedText = recognizedText.replaceAll("[^a-zA-Z0-9]+", " ");
         }
-        if (recognizedText.length() != 0) {
-            ((TextView) findViewById(R.id.field)).setText(recognizedText.trim());
+
+        recognizedText = recognizedText.trim();
+/*
+        if ( recognizedText.length() != 0 ) {
+            _field.setText(_field.getText().toString().length() == 0 ? recognizedText : _field.getText() + " " + recognizedText);
+            _field.setSelection(_field.getText().toString().length());
         }
+*/
+        // Cycle done.
     }
 
-    */
+}
+
